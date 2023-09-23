f80bcd7351a2def7f036c95bcd5b2f77bd4fc93d
naomiinyele <inyeleinyele@gmail.com>
2023-09-23 13:42:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+ customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+//customerName
+//upperCaseCustomerName()
+function setBestCustomer() {
+     bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'initialassignment'
+function changeLeastFavoriteCustomer(){
+leastFavoriteCustomer = 'Assignment to constant variable.'
+}
+changeLeastFavoriteCustomer()
